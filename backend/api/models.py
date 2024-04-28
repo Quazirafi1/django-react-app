87f422f87ffbc9facdc3954f430591ed12116648
Quazi Ghulam Rafi <quazirafi1@gmail.com>
2024-04-28 22:34:27 +0300

    notes create delete done

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Foreign key binds a user's notes to that user, one user can have many different notes
+    # One to many relationship,
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+
+    def __str__(self):
+        return self.title
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
-
+from .models import Note
 # Create your views here.
+
 class CreateUserView(generics.CreateAPIView):
     #get list of User objects (making sure user does not already exists)
     queryset = User.objects.all()
@@ -12,3 +13,28 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     #who can access this (anyone who wants to register)
     permission_classes = [AllowAny]
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    # view only the notes written by user
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+    # only validated users can create note
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+    
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+        # view only the notes written by user
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
